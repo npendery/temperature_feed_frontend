c74d505d2008d761726aa303d20738b655a81544
Nick Pendery <npendery@gmail.com>
2024-01-04 18:05:20 -0700

    Add frontend

--- src/App.js
+++ src/App.js
@@ -1,22 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { useQuery, gql } from '@apollo/client';
 function App() {
+    const GET_CURRENT_TEMPERATURE = gql`
+      query CurrentTemperature {
+        currentTemperature {
+          timestamp
+          value
+        }
+      }
+    `;
+
+  function DisplayCurrentTemperature() {
+    const { loading, error, data } = useQuery(GET_CURRENT_TEMPERATURE);
+    
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error : {error.message}</p>;
+  
+    const date = new Date(data.currentTemperature.timestamp);
+    const formattedDate = date.toLocaleDateString('en-US', {
+      weekday: 'long',
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+    });
+    const formattedTime = date.toLocaleTimeString('en-US');
+
+    
+    return (
+      <div>
+        <p>
+          The most recent temperature reading is {data.currentTemperature.value.toFixed(0)}°F on{' '}
+          {formattedDate} at {formattedTime}.
+        </p>
+      </div>
+    );
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <DisplayCurrentTemperature />
       </header>
     </div>
   );
